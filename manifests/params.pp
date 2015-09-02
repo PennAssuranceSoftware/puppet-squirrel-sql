483cd36758b32a7adc752866fe821b82027d7f69
Jerome Bridge <jeromebridge@gmail.com>
2015-09-02 11:15:48 -0400

    First iteration of Puppet Squirrel SQL Client

---
+++ manifests/params.pp
@@ -0,0 +1,10 @@
+# Class: squirrel_sql::params
+#
+# This class defines some parameters
+#
+class squirrel_sql::params {
+
+  $target_dir = '/opt'
+  $download_bin = "${target_dir}/squirrel_sql/squirrel-sql.sh"
+
+}
